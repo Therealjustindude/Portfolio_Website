3467cfcf79a34db2533a4e513bb8941edcbedfe1
therealjustindude <therealjustindude@gmail.com>
2020-12-03 15:46:06 -0600

    initial commit - navbar displays Hello People at the moment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Suspense, lazy } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+
+const Home = lazy(() => import('./components/Home'));
+const Navbar = lazy(() => import('./components/Navbar'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" render={(routerProps) => <Home history={routerProps} />}
+        />
+      </Switch>
+    </Suspense>
   );
 }
 
